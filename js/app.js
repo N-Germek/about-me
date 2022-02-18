bce9b1d9776261cdff9e9af847d7834a6f901b35
Natalija Germek <n.germek@icloud.com>
2022-02-17 23:16:47 -0800

    added JS code

---
+++ js/app.js
@@ -0,0 +1,11 @@
+<use strict>
+<script>
+    // console.log("Hello World");
+    alert('Hello'); 
+    let username = prompt('What is your name?');
+
+    alert('Hello ' + username);
+    console.log(username);
+    // console.log(tested username prompt in JS)
+
+</script>
