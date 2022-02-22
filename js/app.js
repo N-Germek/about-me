6feb27b92ae0d2be80ae75c2fe21ae4fc6dc0316
Natalija Germek <n.germek@icloud.com>
2022-02-21 22:25:39 -0800

    updated JS number guessing game and notes

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 'use strict';
 
+let score = 0;
+// global variable can be used anywhere in the code. local variables can only be used for their contained function. usually contained in the top like Head in HTML.
+
+
 //console.log('Hello World');
 // console log finally responded with correct messaging. //
 
-let username = prompt('What is your name?').toUpperCase();
+let username = prompt('What is your name?');
 alert('Hello ' + username) + '!';
 
 let travel = prompt('Do I like to travel?').toLowerCase();
 if (travel === 'yes' || travel === 'y') {
+  score++;
   //console.log('Right on! I love to travel!');
   alert('Right on! I love to travel!');
 }
@@ -24,6 +29,7 @@ else {
 
 let pets = prompt('Do I have pets?').toLowerCase();
 if (pets === 'yes' || pets === 'y') {
+  score++;
   //console.log('Yes and I love my furbabies!');
   alert('Yes and I love my furbabies!');
 }
@@ -42,6 +48,7 @@ else {
 
 let adventures = prompt('Do I like to go on adventures?').toLowerCase();
 if (adventures === 'yes' || adventures === 'y') {
+  score++;
   //console.log('Correct, the more impromptu, the better the adventure!');
   alert('Correct, the more impromptu, the better the adventure!');
 }
@@ -58,6 +65,7 @@ else {
 
 let country = prompt('Have I lived in another country?').toLowerCase();
 if (country === 'yes' || country === 'y') {
+  score++;
   //console.log('You are right! Half of my family still lives outside of the USA.');
   alert('You are right! Half of my family still lives outside of the USA.');
 }
@@ -74,6 +82,7 @@ else {
 
 let newPeople = prompt('Do I like to meet new people?').toLowerCase();
 if (newPeople === 'yes' || newPeople === 'y') {
+  score++;
   //console.log('Of course! New poeple mean new perspectives and things to learn from them!');
   alert('Of course! New poeple mean new perspectives and things to learn from them!');
 }
@@ -90,17 +99,33 @@ else {
 
 // User question to ask user on site will be "Guess which countries I have already visited? Good luck!" //
 
-let score = 0;
-score++;
+// side note: word wrap is moving the line of code to the next line. option z.
+//adding one to the score. like math add the score before giving the answer.
 console.log(score);
+alert('Congratulations, you got ' + score + ' correct!');
 
 // Lines 99 through 109 on are commented out until I can properly make my code work so I don't mess up what I finally understood of my prior code. //
 
 let guesses = 4;
-do {
-  console.log(`You have ${guesses - 1} guesses remaining.`);
-  guesses--;
-} while (guesses > 0);
+let win = false;
+let correctNumber = Math.ceil(Math.random() * 10);
+// Math.random is generating random number as answer and math.ceil tells the random number to round up to the nearest whole number. Math.floor rounds down. adding the * (multiplication) and the 10 tells that it will be between 0 and 10.
+while (guesses && !win) {
+  let userGuess = prompt('I\'m thinking of a number between 1 and 10');
+  if (userGuess === correctNumber) {
+    alert('You got it!');
+    win = true;
+  }
+  else if (userGuess < correctNumber) {
+    alert('Sorry ' + userGuess + ' Too low.');
+    guesses--;
+    alert(`You have ${guesses} guesses remaining.`);
+  }
+  else if (userGuess > correctNumber) {
+    guesses--;
+    alert('Sorry ' + userGuess + ' Too high.');
+    alert(`You have ${guesses} guesses remaining.`);
+  }
+}
 
-console.log('Sorry, that number is too high');
 // Code for  for array with answer to "What countries have I visited?" let FavPlaces = ['Croatia', 'Canada', 'Germany', 'Carribean', 'United States'];
